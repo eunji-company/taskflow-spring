4a11935580eb795fb9825f2dcbb169c2c6024d93
abko <abko121115@gmail.com>
2025-09-03 17:10:15 +0900

    fix: UserRole enum에 getKey 메서드 구현하여 "ROLE_" 접두사 추가

--- src/main/java/indiv/abko/taskflow/domain/user/entity/UserRole.java
+++ src/main/java/indiv/abko/taskflow/domain/user/entity/UserRole.java
@@ -1,13 +1,15 @@
 package indiv.abko.taskflow.domain.user.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
 @AllArgsConstructor
 public enum UserRole {
 	USER("USER"),
 	ADMIN("ADMIN");
 
 	private final String key;
+
+	public String getKey() {
+		return "ROLE_" + key;
+	}
 }
