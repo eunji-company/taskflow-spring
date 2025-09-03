14ce29e79b98e21c23c689812becc22f28d42db8
abko <abko121115@gmail.com>
2025-09-03 16:54:25 +0900

    docs: TODO 주석 추가

--- src/main/java/indiv/abko/taskflow/domain/task/service/TaskServiceApi.java
+++ src/main/java/indiv/abko/taskflow/domain/task/service/TaskServiceApi.java
@@ -3,6 +3,7 @@ package indiv.abko.taskflow.domain.task.service;
 import indiv.abko.taskflow.domain.task.entity.Task;
 
 public interface TaskServiceApi {
+	// TODO 작업해주세요~
 	default Task getByIdOrThrow(long taskId) {
 		// 기본키를 사용하여 Task를 가져온다
 		// 없으면 예외처리
