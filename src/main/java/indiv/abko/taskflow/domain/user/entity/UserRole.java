5f36013363d698369018f5ad5a2eb67bc9642fcf
abko <abko121115@gmail.com>
2025-09-03 16:55:36 +0900

    feat: UserRole enum에 key 필드 및 생성자 추가

--- src/main/java/indiv/abko/taskflow/domain/user/entity/UserRole.java
+++ src/main/java/indiv/abko/taskflow/domain/user/entity/UserRole.java
@@ -1,6 +1,13 @@
 package indiv.abko.taskflow.domain.user.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum UserRole {
-	USER,
-	ADMIN
+	USER("USER"),
+	ADMIN("ADMIN");
+
+	private final String key;
 }
