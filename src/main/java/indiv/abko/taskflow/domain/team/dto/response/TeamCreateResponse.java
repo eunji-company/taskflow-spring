449d5441a75b6aa3c1803e67fa28f37653197a64
eunji-0508 <cojia@naver.com>
2025-09-04 17:54:03 +0900

    style: 코드 스타일 변경

--- src/main/java/indiv/abko/taskflow/domain/team/dto/response/TeamCreateResponse.java
+++ src/main/java/indiv/abko/taskflow/domain/team/dto/response/TeamCreateResponse.java
@@ -3,5 +3,8 @@ package indiv.abko.taskflow.domain.team.dto.response;
 import java.time.LocalDateTime;
 
 public record TeamCreateResponse(
-	Long id, String name, String description, LocalDateTime createdAt) {
+	Long id,
+	String name,
+	String description,
+	LocalDateTime createdAt) {
 }
