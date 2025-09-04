42bcdfdda790da2f4335adb81d9c36be4787981d
김영훈 <hunifive@gmail.com>
2025-09-04 15:46:48 +0900

    fix: 인증/인가 화이트리스트 변경 (#49)

--- src/main/java/indiv/abko/taskflow/global/config/SecurityConfig.java
+++ src/main/java/indiv/abko/taskflow/global/config/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
 			.csrf(AbstractHttpConfigurer::disable)
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(auth -> auth
-				.requestMatchers("/api/auth/**").permitAll()
+				.requestMatchers("/api/auth/register").permitAll()
+				.requestMatchers("/api/auth/login").permitAll()
 				.anyRequest().authenticated()
 			)
 			.oauth2ResourceServer(oauth2 -> oauth2
