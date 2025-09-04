d1e92908f894c7b6bcc9d05a9c49278280de05e9
joon448 <joonjdy448@gmail.com>
2025-09-04 23:33:37 +0900

    feat: Member api - existsByUsername, existsByEmail, createMember 메서드 작성

--- src/main/java/indiv/abko/taskflow/domain/user/repository/MemberRepository.java
+++ src/main/java/indiv/abko/taskflow/domain/user/repository/MemberRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import indiv.abko.taskflow.domain.user.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+	boolean existsByUsername(String username);
+
+	boolean existsByEmail(String email);
 }
