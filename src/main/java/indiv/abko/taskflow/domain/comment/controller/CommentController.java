43f62c85338080fc520556c8c98acf7dbb287191
abko <abko121115@gmail.com>
2025-09-04 12:12:47 +0900

    fix: 댓글 작성 API에서 성공 시 201 Created 상태 코드 반환 수정

--- src/main/java/indiv/abko/taskflow/domain/comment/controller/CommentController.java
+++ src/main/java/indiv/abko/taskflow/domain/comment/controller/CommentController.java
@@ -1,10 +1,12 @@
 package indiv.abko.taskflow.domain.comment.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import indiv.abko.taskflow.domain.comment.dto.command.WriteCommentToTaskCommand;
@@ -23,6 +25,7 @@ public class CommentController {
 	private final WriteCommentToTaskUseCase writeCommentToTaskUseCase;
 
 	@PostMapping("/tasks/{taskId}/comments")
+	@ResponseStatus(HttpStatus.CREATED)
 	public CommonResponse<WriteCommentToTaskResponse> writeComment(@AuthenticationPrincipal AuthMember authMember,
 		@PathVariable("taskId") long taskId,
 		@Valid @RequestBody WriteCommentRequest request) {
