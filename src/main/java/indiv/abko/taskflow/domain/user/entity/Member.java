ab6972d43b09cc4381df2614d0ba3f4379426669
joon448 <joonjdy448@gmail.com>
2025-09-04 16:42:01 +0900

    fix: Member entity의 email에 unique validation 추가

--- src/main/java/indiv/abko/taskflow/domain/user/entity/Member.java
+++ src/main/java/indiv/abko/taskflow/domain/user/entity/Member.java
@@ -35,7 +35,7 @@ public class Member extends BaseTimeEntity {
 	@Column(nullable = false)
 	private String password;
 
-	@Column(nullable = false)
+	@Column(nullable = false, unique = true)
 	private String email;
 
 	@Column(nullable = false)
