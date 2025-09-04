4d7ca5f7851513255e403161626de3a79507f763
abko <abko121115@gmail.com>
2025-09-04 17:57:32 +0900

    fix: DtoConstants에서 TIME_ZONE 상수를 TIME_ZONE_STRING으로 변경

--- src/main/java/indiv/abko/taskflow/global/dto/DtoConstants.java
+++ src/main/java/indiv/abko/taskflow/global/dto/DtoConstants.java
@@ -1,6 +1,9 @@
 package indiv.abko.taskflow.global.dto;
 
+import java.time.ZoneOffset;
+
 public interface DtoConstants {
     public static final String TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
-    public static final String TIME_ZONE = "UTC";
+    public static final String TIME_ZONE_STRING = "UTC";
+    public static final ZoneOffset TIME_ZONE = ZoneOffset.UTC;
 }
