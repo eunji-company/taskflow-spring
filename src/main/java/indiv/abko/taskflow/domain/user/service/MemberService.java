81475e1b1e47cde723cd58ea4042d5d8d84967ba
joon448 <joonjdy448@gmail.com>
2025-09-04 23:33:37 +0900

    feat: Member api - existsByUsername, existsByEmail, createMember 메서드 작성

--- src/main/java/indiv/abko/taskflow/domain/user/service/MemberService.java
+++ src/main/java/indiv/abko/taskflow/domain/user/service/MemberService.java
@@ -1,8 +1,10 @@
 package indiv.abko.taskflow.domain.user.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import indiv.abko.taskflow.domain.user.entity.Member;
+import indiv.abko.taskflow.domain.user.entity.UserRole;
 import indiv.abko.taskflow.domain.user.exception.MemberErrorCode;
 import indiv.abko.taskflow.domain.user.repository.MemberRepository;
 import indiv.abko.taskflow.global.exception.BusinessException;
@@ -13,8 +15,25 @@ import lombok.RequiredArgsConstructor;
 public class MemberService implements MemberServiceApi {
 	private final MemberRepository memberRepository;
 
+	@Transactional(readOnly = true)
 	public Member getByIdOrThrow(long memberId) {
 		return memberRepository.findById(memberId)
 			.orElseThrow(() -> new BusinessException(MemberErrorCode.MEMBER_NOT_FOUND));
+	}
+
+	@Transactional
+	public Member createMember(String username, String password, String email, String name) {
+		Member member = Member.of(username, password, email, name, UserRole.USER);
+		return memberRepository.save(member);
+	}
+
+	@Transactional(readOnly = true)
+	public boolean existsByUsername(String username) {
+		return memberRepository.existsByUsername(username);
+	}
+
+	@Transactional(readOnly = true)
+	public boolean existsByEmail(String email) {
+		return memberRepository.existsByEmail(email);
 	}
 }
--- src/main/java/indiv/abko/taskflow/domain/user/service/MemberServiceApi.java
+++ src/main/java/indiv/abko/taskflow/domain/user/service/MemberServiceApi.java
@@ -4,4 +4,10 @@ import indiv.abko.taskflow.domain.user.entity.Member;
 
 public interface MemberServiceApi {
 	Member getByIdOrThrow(long memberId);
+
+	Member createMember(String username, String password, String email, String name);
+
+	boolean existsByUsername(String username);
+
+	boolean existsByEmail(String email);
 }
