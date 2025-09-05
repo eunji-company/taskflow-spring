e0f37685ec82111af7df00480cb7870253fcf0b3
abko <abko121115@gmail.com>
2025-09-06 01:37:44 +0900

    fix: 불필요한 코드 제거 및 정리

--- src/main/java/indiv/abko/taskflow/domain/task/controller/TaskController.java
+++ src/main/java/indiv/abko/taskflow/domain/task/controller/TaskController.java
@@ -1,10 +1,8 @@
 package indiv.abko.taskflow.domain.task.controller;
 
 import indiv.abko.taskflow.domain.task.dto.reqeust.CreateTaskRequest;
-import indiv.abko.taskflow.domain.task.dto.response.TaskListResponse;
 import indiv.abko.taskflow.domain.task.dto.response.CreateTaskResponse;
 import indiv.abko.taskflow.domain.task.service.CreateTaskUseCase;
-import indiv.abko.taskflow.domain.task.service.TaskListUseCase;
 import indiv.abko.taskflow.global.auth.AuthMember;
 import indiv.abko.taskflow.global.dto.CommonResponse;
 import lombok.RequiredArgsConstructor;
@@ -18,28 +16,27 @@ import org.springframework.web.bind.annotation.*;
 public class TaskController {
 
 	private final CreateTaskUseCase createTaskUseCase;
-	private final TaskListUseCase taskListUseCase;
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public CommonResponse<CreateTaskResponse> createTask(
-			@AuthenticationPrincipal AuthMember authMember,
-			@RequestBody CreateTaskRequest request
-	) {
+		@AuthenticationPrincipal
+		AuthMember authMember,
+		@RequestBody
+		CreateTaskRequest request) {
 		CreateTaskResponse response = createTaskUseCase.execute(authMember, request);
 		return CommonResponse.success("Task가 생성되었습니다.", response);
 	}
 
-
-//	@GetMapping
-//	@ResponseStatus(HttpStatus.OK)
-//	public CommonResponse<Page<TaskListResponse>> taskList(
-//			@AuthenticationPrincipal AuthMember authMember,
-//			@RequestParam(defaultValue = "0") int page,
-//			@RequestParam(defaultValue = "10") int size
-//	){
-//		Page<TaskListResponse> response = taskListUseCase.taskList(page, size);
-//		return CommonResponse.success("Task 목록을 조회했습니다.", response);
-//	}
+	//	@GetMapping
+	//	@ResponseStatus(HttpStatus.OK)
+	//	public CommonResponse<Page<TaskListResponse>> taskList(
+	//			@AuthenticationPrincipal AuthMember authMember,
+	//			@RequestParam(defaultValue = "0") int page,
+	//			@RequestParam(defaultValue = "10") int size
+	//	){
+	//		Page<TaskListResponse> response = taskListUseCase.taskList(page, size);
+	//		return CommonResponse.success("Task 목록을 조회했습니다.", response);
+	//	}
 
 }
