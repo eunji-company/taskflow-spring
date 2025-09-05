694b76a18956e180caa165167ee19317e0f1784b
eunji-0508 <cojia@naver.com>
2025-09-05 12:40:30 +0900

    fix: 팀 수정 기능 수정

--- src/main/java/indiv/abko/taskflow/domain/team/dto/response/UpdateTeamResponse.java
+++ src/main/java/indiv/abko/taskflow/domain/team/dto/response/UpdateTeamResponse.java
@@ -1,10 +1,21 @@
 package indiv.abko.taskflow.domain.team.dto.response;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record UpdateTeamResponse(
 	Long id,
 	String name,
 	String description,
-	LocalDateTime createdAt) {
+	LocalDateTime createdAt,
+	List<UserResp> members
+) {
+	public record UserResp(
+		long id,
+		String username,
+		String name,
+		String email,
+		String role,
+		LocalDateTime createdAt) {
+	}
 }
