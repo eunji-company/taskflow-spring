f59dffe4c77c20b1b2b16d9c8bf38afb0652e0ce
eunji-0508 <cojia@naver.com>
2025-09-05 11:57:03 +0900

    fix: 팀 생성 기능 수정

--- src/main/java/indiv/abko/taskflow/domain/team/dto/response/CreateTeamResponse.java
+++ src/main/java/indiv/abko/taskflow/domain/team/dto/response/CreateTeamResponse.java
@@ -1,10 +1,12 @@
 package indiv.abko.taskflow.domain.team.dto.response;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record CreateTeamResponse(
 	Long id,
 	String name,
 	String description,
-	LocalDateTime createdAt) {
+	LocalDateTime createdAt,
+	List<Void> members) {
 }
