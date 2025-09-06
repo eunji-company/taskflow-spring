fa05f8c84e266e8a89eeee2d8640474b8f05b98b
eunji-0508 <cojia@naver.com>
2025-09-06 15:16:35 +0900

    fix: 검증 어노테이션을 @NotBlank에서 @NotNull로 수정

--- src/main/java/indiv/abko/taskflow/domain/team/dto/request/AddTeamMemberRequest.java
+++ src/main/java/indiv/abko/taskflow/domain/team/dto/request/AddTeamMemberRequest.java
@@ -1,7 +1,7 @@
 package indiv.abko.taskflow.domain.team.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record AddTeamMemberRequest(
-	@NotBlank(message = "사용자 ID 입력은 필수입니다.") Long userId) {
+	@NotNull(message = "사용자 ID 입력은 필수입니다.") Long userId) {
 }
