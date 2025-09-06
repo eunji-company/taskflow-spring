c4b2b8401a7e1e71cc97f91da256c23f09401df3
eunji-0508 <cojia@naver.com>
2025-09-06 21:51:58 +0900

    refactor: for-each 반복문을 stream으로 변환

--- src/main/java/indiv/abko/taskflow/domain/team/service/DeleteTeamMemberUseCase.java
+++ src/main/java/indiv/abko/taskflow/domain/team/service/DeleteTeamMemberUseCase.java
@@ -1,14 +1,13 @@
 package indiv.abko.taskflow.domain.team.service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import indiv.abko.taskflow.domain.team.dto.response.DeleteTeamMemberResponse;
 import indiv.abko.taskflow.domain.team.entity.Team;
-import indiv.abko.taskflow.domain.team.entity.TeamMember;
 import indiv.abko.taskflow.domain.team.entity.TeamMemberId;
 import indiv.abko.taskflow.domain.team.exception.TeamErrorCode;
 import indiv.abko.taskflow.domain.team.repository.TeamMemberRepository;
@@ -44,18 +43,17 @@ public class DeleteTeamMemberUseCase {
 
 		team.deleteMember(member);
 
-		List<DeleteTeamMemberResponse.UserResp> resps = new ArrayList<>();
-
-		for (TeamMember teamMember : team.getTeamMembers()) {
-			Member member1 = teamMember.getId().getMember();
-			resps.add(new DeleteTeamMemberResponse.UserResp(
+		List<DeleteTeamMemberResponse.UserResp> resps = team.getTeamMembers()
+			.stream()
+			.map(teamMember -> teamMember.getId().getMember())
+			.map(member1 -> new DeleteTeamMemberResponse.UserResp(
 				member1.getId(),
 				member1.getUsername(),
 				member1.getName(),
 				member1.getEmail(),
 				member1.getUserRole().name(),
-				member1.getCreatedAt()));
-		}
+				member1.getCreatedAt()))
+			.collect(Collectors.toList());
 
 		return new DeleteTeamMemberResponse(
 			team.getId(),
