17388dccc574b6a012a0d49c613026c612909c1c
joon448 <joonjdy448@gmail.com>
2025-09-07 18:14:14 +0900

    feat: Member Api 작성 (existsById, withdraw)

--- src/main/java/indiv/abko/taskflow/domain/user/service/MemberServiceApi.java
+++ src/main/java/indiv/abko/taskflow/domain/user/service/MemberServiceApi.java
@@ -1,8 +1,8 @@
 package indiv.abko.taskflow.domain.user.service;
 
-import indiv.abko.taskflow.domain.user.entity.Member;
-
 import java.util.Optional;
+
+import indiv.abko.taskflow.domain.user.entity.Member;
 
 public interface MemberServiceApi {
 
@@ -14,20 +14,9 @@ public interface MemberServiceApi {
 
 	boolean existsByEmail(String email);
 
-	// TODO: 구현해주세요 혜준님
-	default boolean exstsById(long memberId) {
-		//id가 존재한다면 true, 없다면 false
-		return true;
-	}
+	boolean existsById(long memberId);
 
 	Optional<Member> findByUsername(String username);
 
-	/*
-	TODO: 혜준님 필독!
-	MEMBER Entity에 @SQLDelete로 Soft delete 처리 해뒀으니
-	그냥 memberRepository delete 메서드 쓰시면 됩니다!
-	 */
-	default void withdraw(Member member) {
-		// member로 회원탈퇴 처리
-	}
+	void withdraw(Member member);
 }
